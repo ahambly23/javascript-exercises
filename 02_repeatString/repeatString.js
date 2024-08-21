dbbf1a2fa0cbcce43801e15334f1fec302a99e00
ahambly23 <ashleyhambly4@hotmail.com>
2024-08-21 13:10:48 +0100

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    let loopString = '';
+
+    for (let x = 0; x < num; x++) {
+        loopString = loopString + string;
+    }
+
+    return loopString;
 };
 
 // Do not edit below this line
