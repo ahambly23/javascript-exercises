21e8eaa4577d3243ba3ec0404c4f6f2a45183317
ahambly23 <ashleyhambly4@hotmail.com>
2024-08-21 14:01:09 +0100

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+
+    let removeArr = remove;
+
+    for (let x = 0; x < arr.length; x++) {
+        for (let y = 0; y < removeArr.length; y++) {
+            if (arr[x] === removeArr[y]) {
+                arr.splice(x, 1);
+                x--;
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
