a99c54fab7eaf9b297e8a8b55a3cb3cdc40784d0
ahambly23 <ashleyhambly4@hotmail.com>
2024-08-22 19:28:56 +0100

    Completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+// If year % 400 == 0 TRUE
+// If year % 100 == 0 but year % 400 != 0 FALSE
+// If year % 4 == 0 TRUE
 
-};
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+        return true;
+    } else if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
