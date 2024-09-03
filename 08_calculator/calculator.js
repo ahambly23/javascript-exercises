4ecdbe7fc419974e6b5cf20ccc335e5299d547cf
ahambly23 <ashleyhambly4@hotmail.com>
2024-09-03 18:30:06 +0100

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let x = 0; x < array.length; x++) {
+    sum += array[x];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiply = array[0];
+  for (let x = 1; x < array.length; x++) {
+    multiply *= array[x];
+  }
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let power = a;
+	for (let x = 0; x < (b - 1); x++) {
+    power *= a;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = a;
+
+  if (a === 0) {
+    return 1;
+  } else {
+    for (let x = (a - 1); x > 0; x--) {
+      factorial *= x;
+    }
+  }
+  return factorial;
 };
 
 // Do not edit below this line
