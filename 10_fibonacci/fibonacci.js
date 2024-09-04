eb0115cbdcfbda28fbee4ab437815d353990bcad
ahambly23 <ashleyhambly4@hotmail.com>
2024-09-04 14:05:24 +0100

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    // create an array which holds the fibonacci sequence
+    let fibonacci = [0, 1];
+    if (number >= 0) {
+        for (let x = 2; x <= number; x++) {
+            fibonacci[x] = fibonacci[x - 2] + fibonacci[x - 1];
+        }
+    // Loop through the array, adding the two numbers before number
+    } else {
+        return "OOPS"
+    }
+    return fibonacci[number];
 };
 
 // Do not edit below this line
