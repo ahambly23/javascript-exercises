4d3850fbe8447e4e3b71bcefb5e91793f7a5ef24
ahambly23 <ashleyhambly4@hotmail.com>
2024-09-08 00:01:56 +0100

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(object) {
 
+    const ages = object.map(person => {
+        const currentYear = new Date().getFullYear();
+        const age = person.yearOfDeath ? (person.yearOfDeath - person.yearOfBirth) : (currentYear - person.yearOfBirth);
+        return age;
+    })
+
+    const maxAge = Math.max(...ages);
+
+    const oldestPerson = object.find(person => {
+        const currentYear = new Date().getFullYear();
+        const age = person.yearOfDeath ? (person.yearOfDeath - person.yearOfBirth) : (currentYear - person.yearOfBirth);
+        return age === maxAge
+    })
+    
+    return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
